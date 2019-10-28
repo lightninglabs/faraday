e91b22480f0588575c748e4edfc9575df6528f80
carla <kirkcohenc@gmail.com>
2019-10-28 15:29:20 +0200

    terminator: Connect to LND RPC
    This commit adds code to connect to LND's RPC from the
    terminator main function. It allows users to specify
    config through command line flags, and validates the
    input where possible.

---
+++ config.go
@@ -0,0 +1,78 @@
+package terminator
+
+import (
+	"fmt"
+
+	"github.com/jessevdk/go-flags"
+)
+
+const (
+	defaultRPCPort     = "10009"
+	defaultRPCHostPort = "localhost:" + defaultRPCPort
+	defaultMacaroon    = "admin.macaroon"
+	defaultNetwork     = "mainnet"
+)
+
+type config struct {
+	// RPCServer is host:port that lnd's RPC server is listening on.
+	RPCServer string `long:"rpcserver" description:"host:port that LND is listening for RPC connections on"`
+
+	// MacaroonDir is the directory containing macaroons.
+	MacaroonDir string `long:"macaroondir" description:"Dir containing macaroons"`
+
+	// MacaroonFile is the file name of the macaroon to use.
+	MacaroonFile string `long:"macaroonfile" description:"Macaroon file to use."`
+
+	// TLSCertPath is the path to the tls cert that terminator should use.
+	TLSCertPath string `long:"tlscertpath" description:"Path to TLS cert"`
+
+	// TestNet is set to true when running on testnet.
+	TestNet bool `long:"testnet" description:"Use the testnet network"`
+
+	// Simnet is set to true when using btcd's simnet.
+	Simnet bool `long:"simnet" description:"Use simnet"`
+
+	// Simnet is set to true when using bitcoind's regtest.
+	Regtest bool `long:"regtest" description:"Use regtest"`
+
+	// network is a string containing the network we're running on.
+	network string
+}
+
+// loadConfig starts with a skeleton default config, and reads in user provided
+// configuration from the command line. It does not provide a full set of
+// defaults or validate user input because validation and sensible default
+// setting are performed by the lndclient package.
+func loadConfig() (*config, error) {
+	// Start with a default config.
+	config := &config{
+		RPCServer:    defaultRPCHostPort,
+		network:      defaultNetwork,
+		MacaroonFile: defaultMacaroon,
+	}
+
+	// Parse command line options to obtain user specified values.
+	if _, err := flags.Parse(config); err != nil {
+		return nil, err
+	}
+
+	var netCount int
+	if config.TestNet {
+		config.network = "testnet"
+		netCount++
+	}
+	if config.Regtest {
+		config.network = "regtest"
+		netCount++
+	}
+	if config.Simnet {
+		config.network = "simnet"
+		netCount++
+	}
+
+	if netCount > 1 {
+		return nil, fmt.Errorf("do not specify more than one network flag")
+	}
+
+	return config, nil
+}
--- terminator.go
+++ terminator.go
@@ -1,10 +1,44 @@
 // Package terminator contains the main function for the terminator.
 package terminator
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/lightninglabs/loop/lndclient"
+	"github.com/lightningnetwork/lnd/lnrpc"
+)
+
 // Main is the real entry point for terminator. It is required to ensure that
 // defers are properly executed when os.Exit() is called.
 func Main() error {
-	log.Infof("That is all. I will be back.")
+	config, err := loadConfig()
+	if err != nil {
+		return fmt.Errorf("error loading config: %v", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// NewBasicClient get a lightning rpc client with
+	client, err := lndclient.NewBasicClient(
+		config.RPCServer,
+		config.TLSCertPath,
+		config.MacaroonDir,
+		config.network,
+		lndclient.MacFilename(config.MacaroonFile),
+	)
+	if err != nil {
+		return fmt.Errorf("cannot connect to lightning client: %v", err)
+	}
+
+	channels, err := client.ListChannels(ctx, &lnrpc.ListChannelsRequest{})
+	if err != nil {
+		return fmt.Errorf("error calling list channels: %v", err)
+	}
+
+	log.Infof("Found %v channels, that's all for now. I will be back.",
+		len(channels.Channels))
 
 	return nil
 }

9c664f24216f952eac7e170f487f84ff76c54965
carla <kirkcohenc@gmail.com>
2019-12-16 10:46:18 +0200

    recommend: add log for subsystem

--- log.go
+++ log.go
@@ -2,20 +2,26 @@ package terminator
 
 import (
 	"github.com/btcsuite/btclog"
+	"github.com/lightninglabs/terminator/recommend"
 	"github.com/lightningnetwork/lnd/build"
 )
 
 // Subsystem defines the logging code for this subsystem.
 const Subsystem = "TERM"
 
-// log is a logger that is initialized with no output filters. This
-// means the package will not perform any logging by default until the
-// caller requests it.
-var log btclog.Logger
+var (
+	logWriter = build.NewRotatingLogWriter()
+
+	// log is a logger that is initialized with no output filters. This
+	// means the package will not perform any logging by default until the
+	// caller requests it.
+	log = build.NewSubLogger(Subsystem, logWriter.GenSubLogger)
+)
 
 // The default amount of logging is none.
 func init() {
-	UseLogger(build.NewSubLogger(Subsystem, nil))
+	setSubLogger(Subsystem, log, nil)
+	addSubLogger(recommend.Subsystem, recommend.UseLogger)
 }
 
 // UseLogger uses a specified Logger to output package logging info.
@@ -23,4 +29,22 @@ func init() {
 // using btclog.
 func UseLogger(logger btclog.Logger) {
 	log = logger
+}
+
+// addSubLogger is a helper method to conveniently create and register the
+// logger of a sub system.
+func addSubLogger(subsystem string, useLogger func(btclog.Logger)) {
+	logger := build.NewSubLogger(subsystem, logWriter.GenSubLogger)
+	setSubLogger(subsystem, logger, useLogger)
+}
+
+// setSubLogger is a helper method to conveniently register the logger of a sub
+// system.
+func setSubLogger(subsystem string, logger btclog.Logger,
+	useLogger func(btclog.Logger)) {
+
+	logWriter.RegisterSubLogger(subsystem, logger)
+	if useLogger != nil {
+		useLogger(logger)
+	}
 }

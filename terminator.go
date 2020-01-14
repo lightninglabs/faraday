ce12b973a0e7913f140bdb16b5de841cc4b02dcb
carla <kirkcohenc@gmail.com>
2020-01-14 10:12:12 +0200

    terminator: log insights and channel revenue

--- terminator.go
+++ terminator.go
@@ -4,9 +4,12 @@ package terminator
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/lightninglabs/loop/lndclient"
+	"github.com/lightninglabs/terminator/insights"
 	"github.com/lightninglabs/terminator/recommend"
+	"github.com/lightninglabs/terminator/revenue"
 	"github.com/lightningnetwork/lnd/lnrpc"
 )
 
@@ -30,7 +33,77 @@ func Main() error {
 		lndclient.MacFilename(config.MacaroonFile),
 	)
 	if err != nil {
-		return fmt.Errorf("cannot connect to lightning client: %v", err)
+		return fmt.Errorf("cannot connect to lightning "+
+			"client: %v", err)
+	}
+
+	openChannels, err := client.ListChannels(
+		ctx, &lnrpc.ListChannelsRequest{},
+	)
+	if err != nil {
+		return err
+	}
+
+	// Get a revenue report for the period specified.
+	revenueReport, err := revenue.GetRevenueReport(&revenue.Config{
+		OpenChannels: openChannels.Channels,
+		ClosedChannels: func() ([]*lnrpc.ChannelCloseSummary, error) {
+			resp, err := client.ClosedChannels(
+				ctx, &lnrpc.ClosedChannelsRequest{},
+			)
+			if err != nil {
+				return nil, err
+			}
+
+			return resp.Channels, nil
+		},
+		ForwardingHistory: func(offset, maxEvents uint32) (
+			events []*lnrpc.ForwardingEvent, u uint32, e error) {
+			// Query for events from the revenue period offset
+			// until the present.
+			start := uint64(
+				time.Now().Add(config.RevenuePeriod * -1).Unix(),
+			)
+
+			resp, err := client.ForwardingHistory(
+				ctx, &lnrpc.ForwardingHistoryRequest{
+					StartTime:    start,
+					EndTime:      uint64(time.Now().Unix()),
+					IndexOffset:  offset,
+					NumMaxEvents: maxEvents,
+				},
+			)
+			if err != nil {
+				return nil, 0, err
+			}
+			return resp.ForwardingEvents, resp.LastOffsetIndex, nil
+		},
+	})
+	if err != nil {
+		return err
+	}
+	log.Info(revenueReport)
+
+	// Gather relevant insights on all channels.
+	channels, err := insights.GetChannels(&insights.Config{
+		OpenChannels: openChannels.Channels,
+		CurrentHeight: func() (u uint32, e error) {
+			resp, err := client.GetInfo(ctx,
+				&lnrpc.GetInfoRequest{})
+			if err != nil {
+				return 0, err
+			}
+
+			return resp.BlockHeight, nil
+		},
+		RevenueReport: revenueReport,
+	})
+	if err != nil {
+		return err
+	}
+
+	for _, channel := range channels {
+		log.Info(channel)
 	}
 
 	// Get channel close recommendations for the current set of open public

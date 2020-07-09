b8eef9c5105c82509ddd946be3951bfb395db118
carla <kirkcohenc@gmail.com>
2020-07-09 17:15:57 +0200

    frdrpc: surface bitcoin price and timestamp in report

--- frdrpc/node_report.go
+++ frdrpc/node_report.go
@@ -60,6 +60,10 @@ func rpcReportResponse(report accounting.Report) (*NodeReportResponse,
 			Fiat:      entry.FiatValue.String(),
 			Reference: entry.Reference,
 			Note:      entry.Note,
+			BtcPrice: &BitcoinPrice{
+				Price:          entry.BTCPrice.Price.String(),
+				PriceTimestamp: uint64(entry.BTCPrice.Timestamp.Unix()),
+			},
 		}
 
 		rpcType, err := rpcEntryType(entry.Type)

49ddecfbec5d7632bf8a45d7495cf806645d2253
Oliver Gugger <gugger@gmail.com>
2021-04-21 15:35:43 +0200

    frdrpc: use rest-annotations.yaml for REST definitions
    To simplify the RPC generation and make it easier for people to compile
    the faraday.proto file, we extract the REST definitions into their own
    file.

--- frdrpc/gen_protos.sh
+++ frdrpc/gen_protos.sh
@@ -3,19 +3,15 @@
 echo "Generating faraday gRPC server protos"
 
 # Generate the protos.
-protoc -I/usr/local/include -I. \
-       -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-       --go_out=plugins=grpc,paths=source_relative:. \
-       faraday.proto
+protoc --go_out=plugins=grpc,paths=source_relative:. \
+  faraday.proto
 
 # Generate the REST reverse proxy.
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --grpc-gateway_out=logtostderr=true,paths=source_relative:. \
+protoc \
+  --grpc-gateway_out=logtostderr=true,paths=source_relative,grpc_api_configuration=rest-annotations.yaml:. \
   faraday.proto
 
 # Finally, generate the swagger file which describes the REST API in detail.
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --swagger_out=logtostderr=true:. \
+protoc \
+  --swagger_out=logtostderr=true,grpc_api_configuration=rest-annotations.yaml:. \
   faraday.proto

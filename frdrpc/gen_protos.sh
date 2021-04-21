055b39cfe943dbb2cc29c965553d89dc4e3b8372
Oliver Gugger <gugger@gmail.com>
2021-04-21 15:35:41 +0200

    frdrpc: rename rpc.proto to faraday.proto
    Because the file name "rpc.proto" is already taken by lnd, we get a
    weird warning everywhere we include both the lnd and faraday, as
    libraries for example:
    WARNING: proto: file "rpc.proto" is already registered
    
    We fix this problem by renaming faraday's proto file.

--- frdrpc/gen_protos.sh
+++ frdrpc/gen_protos.sh
@@ -6,16 +6,16 @@ echo "Generating faraday gRPC server protos"
 protoc -I/usr/local/include -I. \
        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
        --go_out=plugins=grpc,paths=source_relative:. \
-       rpc.proto
+       faraday.proto
 
 # Generate the REST reverse proxy.
 protoc -I/usr/local/include -I. \
   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
   --grpc-gateway_out=logtostderr=true,paths=source_relative:. \
-  rpc.proto
+  faraday.proto
 
 # Finally, generate the swagger file which describes the REST API in detail.
 protoc -I/usr/local/include -I. \
   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
   --swagger_out=logtostderr=true:. \
-  rpc.proto
+  faraday.proto
